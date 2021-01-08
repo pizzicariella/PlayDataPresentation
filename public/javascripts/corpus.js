cd8fc0901440f8e7a3353c1475a9514a27bf5a0f
Marie Kemker <s0558478@htw-berlin.de>
2021-01-08 09:50:03 +0100

    navbarSelection method not working yet

--- public/javascripts/corpus.js
+++ public/javascripts/corpus.js
@@ -198,3 +198,23 @@ function createArticleInformation(tagPercentage, article) {
     }
 }
 
+/*function navbarSelection(){
+    console.log("clicked");
+    const navbarGrey = document.getElementsByClassName("w3-theme-l1");
+    console.log(navbarGrey.item(1))
+    const navbarBlack = document.getElementsByClassName("w3-bar-item w3-button w3-hide-small w3-hover-white");
+    for(let i; i<navbarGrey.length; i++){
+        console.log("infor")
+        console.log(navbarGrey.item(i))
+        navbarGrey.item(i).class = "w3-bar-item w3-button w3-hide-small w3-hover-white";
+    }
+    for(let i; i<navbarBlack.length; i++){
+        if(navbarBlack.item(i).innerText == "Corpus"){
+            navbarBlack.item(i).class = "w3-bar-item w3-button w3-theme-l1"
+        }
+    }
+}*/
+
+//navbarSelection()
+
+
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,18 @@
 function navbarSelection(){
     console.log("clicked");
-    //el.class = "w3-bar-item w3-button w3-theme-l1";
-    return true;
+    const navbarGrey = document.getElementsByClassName("w3-theme-l1");
+    console.log(navbarGrey)
+    const navbarBlack = document.getElementsByClassName("w3-bar-item w3-button w3-hide-small w3-hover-white");
+    for(let i; i<navbarGrey.length; i++){
+        console.log("infor")
+        console.log(navbarGrey.item(i))
+        navbarGrey.item(i).class = "w3-bar-item w3-button w3-hide-small w3-hover-white";
+    }
+    for(let i; i<navbarBlack.length; i++){
+        if(navbarBlack.item(i).innerText == "Corpus"){
+            navbarBlack.item(i).class = "w3-bar-item w3-button w3-theme-l1"
+        }
+    }
 }
+//function muss später aufgerufen werden, da liste hier noch nicht existiert
+navbarSelection()
